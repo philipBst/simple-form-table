2e2c43dea78bce9212d916426106e48b668a9292
Philip Basnet <philipbasnet@gmail.com>
2022-12-05 17:23:44 +0545

    feat: create crud services for products

--- src/services/products.service.ts
+++ src/services/products.service.ts
@@ -1,5 +1,10 @@
 import { PRODUCT_BASE_ENDPOINT } from "../api/endpoints";
-import { getRequest } from "../api/methods";
+import {
+  deleteRequest,
+  getRequest,
+  postRequest,
+  putRequest,
+} from "../api/methods";
 
 import { IProduct } from "../interfaces";
 
@@ -10,5 +15,38 @@ export async function getAllProducts() {
   } catch (error) {
     // handle error
     return [];
+  }
+}
+
+export async function createNewProduct(product: IProduct) {
+  try {
+    const response = await postRequest(PRODUCT_BASE_ENDPOINT, product);
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function updateProductById(
+  productId: Pick<IProduct, "id">["id"],
+  product: IProduct
+) {
+  try {
+    const response = await putRequest(
+      PRODUCT_BASE_ENDPOINT + productId,
+      product
+    );
+    return response;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export async function deleteProductById(productId: Pick<IProduct, "id">["id"]) {
+  try {
+    const response = await deleteRequest(PRODUCT_BASE_ENDPOINT + productId);
+    return response;
+  } catch (error) {
+    console.error(error);
   }
 }
