991b00e291ebd0069e5ccaee02cc697643902385
Philip Basnet <philipbasnet@gmail.com>
2022-12-05 22:41:54 +0545

    feat: integrate store on app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,35 @@
+import { useEffect, Fragment } from "react";
+import { TableContainer, Button, VStack } from "@chakra-ui/react";
+
+import { ProductTable } from "./components";
+
+import { getAllProducts } from "./services/products.service";
+
+import { useProductStore } from "./store/product.store";
+import { openProductDialog } from "./store/productDialog.store";
+import { addProduct } from "./actions/product.actions";
+
+const preFetch = (() => getAllProducts())();
+
 function App() {
-  return <div></div>;
+  const { products, setProducts } = useProductStore();
+
+  useEffect(() => {
+    preFetch.then((data) => setProducts(data));
+  }, []);
+
+  return (
+    <VStack gap={10}>
+      <TableContainer maxWidth={1200} mt={10}>
+        <ProductTable data={products} />
+      </TableContainer>
+      <Button
+        onClick={() => openProductDialog("Add new Item", "Add", {}, addProduct)}
+      >
+        Add new item
+      </Button>
+    </VStack>
+  );
 }
 
 export default App;
