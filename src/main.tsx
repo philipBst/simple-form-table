5a2af39416f4de8cee088c2704852c82b4c64052
Philip Basnet <philipbasnet@gmail.com>
2022-12-05 22:40:12 +0545

    feat: implement chakra  provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { ChakraProvider } from "@chakra-ui/react";
+
 import App from "./App";
+
+import { ProductDialog } from "./components";
+
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <App />
+      <ProductDialog />
+    </ChakraProvider>
   </React.StrictMode>
 );
